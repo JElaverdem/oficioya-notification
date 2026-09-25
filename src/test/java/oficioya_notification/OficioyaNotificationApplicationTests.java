5a053d41efb6e3cb0434ceea498a6434891372e1
Juan Jose Rivera Lopez <juan.rivera@escuelaing.edu.co>
2026-09-24 22:10:42 -0500

    Agrego elementos de constitución de definición de Notificación

---
+++ src/test/java/oficioya_notification/OficioyaNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package oficioya_notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OficioyaNotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
